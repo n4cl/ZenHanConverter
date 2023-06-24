4adc9dc16f8bb5e79bdad03e2e33d05c717e6718
N4CL <devn4cl@gmail.com>
2023-06-24 23:55:20 +0900

    Add: 数字の変換機能を追加 (#4)

--- zen_han_converter/zen_han_convter.py
+++ zen_han_converter/zen_han_convter.py
@@ -1,32 +1,42 @@
-from han_to_zen_table import LATIN_ALPHABET as HAN2ZEN_LATIN_ALPHABET
-from zen_to_han_table import LATIN_ALPHABET as ZEN2HAN_LATIN_ALPHABET
+from han_to_zen_table import LATIN_ALPHABET as HAN2ZEN_LATIN_ALPHABET, ARABIC_NUMERALS as HAN2ZEN_ARABIC_NUMERALS
+from zen_to_han_table import LATIN_ALPHABET as ZEN2HAN_LATIN_ALPHABET, ARABIC_NUMERALS as ZEN2HAN_ARABIC_NUMERALS
 
 
 class BaseConverter:
     """
     コンバーターの基底クラス
     """
-    def __init__(self, latain_alphabet_table):
-        self.latain_alphabet_table = str.maketrans(latain_alphabet_table)
+    def __init__(self, table):
+        self.table = str.maketrans(table)
 
     def convert(self, text):
         """
         変換する
         """
-        return text.translate(self.latain_alphabet_table)
+        return text.translate(self.table)
 
 
 class ZenToHan(BaseConverter):
     """
     全角を半角に変換するクラス
     """
-    def __init__(self):
-        super().__init__(ZEN2HAN_LATIN_ALPHABET)
+    def __init__(self, alphabet_table=True, number_table=True):
+        _table = {}
+        if alphabet_table:
+            _table.update(ZEN2HAN_LATIN_ALPHABET)
+        if number_table:
+            _table.update(ZEN2HAN_ARABIC_NUMERALS)
+        super().__init__(_table)
 
 
 class HanToZen(BaseConverter):
     """
     半角を全角に変換するクラス
     """
-    def __init__(self):
-        super().__init__(HAN2ZEN_LATIN_ALPHABET)
+    def __init__(self, alphabet_table=True, number_table=True):
+        _table = {}
+        if alphabet_table:
+            _table.update(HAN2ZEN_LATIN_ALPHABET)
+        if number_table:
+            _table.update(HAN2ZEN_ARABIC_NUMERALS)
+        super().__init__(_table)
