cb18548a03352ba6c4e4318e03893bec38134ce5
N4CL <devn4cl@gmail.com>
2023-07-08 00:29:23 +0900

    Fix: モジュールをインポートできるように修正 (#19)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zen_han_converter",
-    version="1.0.0",
+    version="1.0.1",
     author="n4cl",
     author_email="devn4cl@gmail.com",
     description="Converts full-width and half-width characters.",
@@ -12,7 +12,11 @@ setup(
     url="https://github.com/n4cl/zen_han_converter.git",
     license='MIT',
     packages=find_packages(),
+    package_dir={"zen_han_converter": "zen_han_converter"},
     classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11"
     ]
 )
