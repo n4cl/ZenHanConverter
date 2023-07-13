c3d5e3cbbb994dc26a7ec45e2d2e812a9e1023ea
n4cl <devn4cl@gmail.com>
2023-07-14 02:17:23 +0900

    Update: 1つクラスで全角、半角を制御できるように更新

--- tests/test_zen_han_converter.py
+++ tests/test_zen_han_converter.py
@@ -1,8 +1,11 @@
-from zen_han_converter.zen_han_converter import ZenToHan, HanToZen
+from zen_han_converter.zen_han_converter import ZenHan
 
 
-def test_ZenToHan():
-    zen_to_han = ZenToHan(alphabet_table=True, number_table=True, ascii_symbol_table=True, space_table=True)
+def test_ZenHan():
+    zen_to_han = ZenHan(alphabet_zen_to_han=True,
+                        number_zen_to_han=True,
+                        ascii_symbol_zen_to_han=True,
+                        space_zen_to_han=True)
     assert zen_to_han.convert('ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ') == 'abcdefghijklmnopqrstuvwxyz'
     assert zen_to_han.convert('abcdefghijklmnopqrstuvwxyz') == 'abcdefghijklmnopqrstuvwxyz'
     assert zen_to_han.convert('０１２３４５６７８９') == '0123456789'
@@ -12,9 +15,10 @@ def test_ZenToHan():
     assert zen_to_han.convert("　") == " "
     assert zen_to_han.convert(" ") == " "
 
-
-def test_HanToZen():
-    han_to_zen = HanToZen(alphabet_table=True, number_table=True, ascii_symbol_table=True, space_table=True)
+    han_to_zen = ZenHan(alphabet_han_to_zen=True,
+                        number_han_to_zen=True,
+                        ascii_symbol_han_to_zen=True,
+                        space_han_to_zen=True)
     assert han_to_zen.convert("abcdefghijklmnopqrstuvwxyz") == "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"
     assert han_to_zen.convert("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ") == "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"
     assert han_to_zen.convert("0123456789") == "０１２３４５６７８９"
--- zen_han_converter/zen_han_converter.py
+++ zen_han_converter/zen_han_converter.py
@@ -3,28 +3,85 @@ from .table import LATIN_ALPHABET, \
                    ASCII_SYMBOL, \
                    SPACE
 
-class BaseConverter:
+
+class ZenHan:
     """
-    コンバーターの基底クラス
+    半角と全角を正規化する
     """
-    def __init__(self, alphabet_table, number_table, ascii_symbol_table, space_table, reverse):
+    def __init__(self,
+                 alphabet_zen_to_han=False,
+                 alphabet_han_to_zen=False,
+                 number_zen_to_han=False,
+                 number_han_to_zen=False,
+                 ascii_symbol_zen_to_han=False,
+                 ascii_symbol_han_to_zen=False,
+                 space_zen_to_han=False,
+                 space_han_to_zen=False):
+        self.validate_init_params(alphabet_zen_to_han,
+                                  alphabet_han_to_zen,
+                                  number_zen_to_han,
+                                  number_han_to_zen,
+                                  ascii_symbol_zen_to_han,
+                                  ascii_symbol_han_to_zen,
+                                  space_zen_to_han,
+                                  space_han_to_zen)
         self.char_to_char_table = {}
 
-        if alphabet_table:
-            _t = self.read_table(LATIN_ALPHABET, reverse)
+        if alphabet_zen_to_han:
+            _t = self.read_table(LATIN_ALPHABET, reverse=True)
             self.char_to_char_table.update(_t)
-        if number_table:
-            _t = self.read_table(ARABIC_NUMERALS, reverse)
-            self.char_to_char_table.update(_t)
-        if ascii_symbol_table:
-            _t = self.read_table(ASCII_SYMBOL, reverse)
-            self.char_to_char_table.update(_t)
-        if space_table:
-            _t = self.read_table(SPACE, reverse)
+        elif alphabet_han_to_zen:
+            _t = self.read_table(LATIN_ALPHABET, reverse=False)
             self.char_to_char_table.update(_t)
 
+        if number_zen_to_han:
+            _t = self.read_table(ARABIC_NUMERALS, reverse=True)
+            self.char_to_char_table.update(_t)
+        elif number_han_to_zen:
+            _t = self.read_table(ARABIC_NUMERALS, reverse=False)
+            self.char_to_char_table.update(_t)
 
-    def read_table(self, table, reverse=False):
+        if ascii_symbol_zen_to_han:
+            _t = self.read_table(ASCII_SYMBOL, reverse=True)
+            self.char_to_char_table.update(_t)
+        elif ascii_symbol_han_to_zen:
+            _t = self.read_table(ASCII_SYMBOL, reverse=False)
+            self.char_to_char_table.update(_t)
+
+        if space_zen_to_han:
+            _t = self.read_table(SPACE, reverse=True)
+            self.char_to_char_table.update(_t)
+        elif space_han_to_zen:
+            _t = self.read_table(SPACE, reverse=False)
+            self.char_to_char_table.update(_t)
+
+    def validate_init_params(self,
+                             alphabet_zen_to_han: bool,
+                             alphabet_han_to_zen: bool,
+                             number_zen_to_han: bool,
+                             number_han_to_zen: bool,
+                             ascii_symbol_zen_to_han: bool,
+                             ascii_symbol_han_to_zen: bool,
+                             space_zen_to_han: bool,
+                             space_han_to_zen: bool):
+        """
+        初期化パラメーターを検証する
+        """
+        assert not (alphabet_zen_to_han is True and alphabet_han_to_zen is True)
+        assert not (number_zen_to_han is True and number_han_to_zen is True)
+        assert not (ascii_symbol_zen_to_han is True and ascii_symbol_han_to_zen is True)
+        assert not (space_zen_to_han is True and space_han_to_zen is True)
+
+        assert not (alphabet_zen_to_han is True \
+           and alphabet_han_to_zen is True \
+           and number_zen_to_han is True \
+           and number_han_to_zen is True \
+           and ascii_symbol_zen_to_han is True \
+           and ascii_symbol_han_to_zen is True \
+           and space_zen_to_han is True \
+           and space_han_to_zen is True)
+
+    def read_table(self, table, reverse=True):
         """
         変換テーブルを読み込む
         """
@@ -44,29 +101,3 @@ class BaseConverter:
             else:
                 result.append(_t)
         return "".join(result)
-
-
-class ZenToHan(BaseConverter):
-    """
-    全角を半角に変換するクラス
-    """
-    def __init__(self,
-                 alphabet_table=True,
-                 number_table=True,
-                 ascii_symbol_table=True,
-                 space_table=True):
-        reverse = True
-        super().__init__(alphabet_table, number_table, ascii_symbol_table, space_table, reverse)
-
-
-class HanToZen(BaseConverter):
-    """
-    半角を全角に変換するクラス
-    """
-    def __init__(self,
-                 alphabet_table=True,
-                 number_table=True,
-                 ascii_symbol_table=True,
-                 space_table=True):
-        reverse = False
-        super().__init__(alphabet_table, number_table, ascii_symbol_table, space_table, reverse)
